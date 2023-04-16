bd62caf9a8d62d24d738f850086583cbe43c62b9
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-04-16 11:48:30 +0530

    Add: to add new contacts to addressbook

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -4,10 +4,9 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Address Book System Program");
-
+		AddressBookSystem addressBookSystem = new AddressBookSystem();
+		addressBookSystem.addContacts();
+		addressBookSystem.displayContacts();
 	}
 
 }
-
-
-
