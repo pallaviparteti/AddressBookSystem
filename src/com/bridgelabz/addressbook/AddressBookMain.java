9fb3e797c272f83090cb694b06356e659ffefb32
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-04-16 11:41:12 +0530

    Add : to crete the contacts

---
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book System Program");
+
+	}
+
+}
+
+
+
